8b8b531d8cb7d1bf64d253161a4ee2b8682d7481
Nicole Sanchez <nicoleabsanchez@gmail.com>
2025-07-21 09:45:09 -0500

    added Credcoins

---
+++ codechef/CREDcoins.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	int x,y;
+	while(t--){
+	    cin >> x >> y;
+	    cout << x * y /100 << endl;
+	}
+	
+	return 0;
+}
