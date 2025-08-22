6888cfa3f1a76db5f09a85c7dca408828d62b525
Nicole Sanchez <nicoleabsanchez@gmail.com>
2025-08-21 20:20:35 -0500

    m

--- leetcode/FindNumberswithEvenNumberofDigits.java
+++ leetcode/FindNumberswithEvenNumberofDigits.java
@@ -2,9 +2,10 @@ class Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
 
-        for (int i=0; i<nums.length; i++) {
-            int digits = (int)(Math.log10(nums[i]) + 1);
-            if (digits % 2 == 0) count++;
+        for (int i = 0; i < nums.length; i++) {
+            int digits = (int) (Math.log10(nums[i]) + 1);
+            if (digits % 2 == 0)
+                count++;
         }
 
         return count;
